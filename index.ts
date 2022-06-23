220dbb4adef377d269c5c4af31979a163ac694b4
zhouwangwang <zhouwangwang@gmail.com>
2022-06-23 12:11:21 +0900

    1 file modified

--- index.ts
+++ index.ts
@@ -1,22 +1,43 @@
-// 分割代入
-let categories = ['post', 'news', 'info', 'other'];
-// let [a, b] = categories;
-// console.log(a, b);
+// 関数
 
-// let [a, b, , d] = categories;
-// console.log(a, b, d);
+// void省略可
+// function greet(name: string): void {
+//   console.log(`Hello world! ${name}`);
+// }
 
-// let [a, b, ...rest] = categories;
-// console.log(a, b, rest);
+// 引数省略可
+// function greet(name?: string) {
+//   console.log(`Hello world! ${name}`);
+// }
 
-// let [a = 10, b = 7] = [1];
-// console.log(a, b);
+// 初期値設定
+function greet(name: string = 'Yohei') {
+  console.log(`Hello world! ${name}`);
+}
 
-let post = { id: 1, content: 'dummy', created: '2020-04-01' };
-// const { id } = post;
-// console.log(id);
+// greet();
 
-// const { id, content: body } = post;
-// console.log(id, body);
+// 可変長引数
+function sum(...values: number[]): number {
+  return values.reduce(function (prev, current) {
+    return prev + current;
+  });
+}
+// console.log(sum(1, 3, 5, 9));
 
-function test({ a, b, c }) {}
+// アロー関数
+let add = (x1: number, x2: number): number => {
+  return x1 + x2;
+};
+// console.log(add(1, 2));
+
+let hello = (name) => `Hello, ${name}`;
+// console.log(hello('Yohei'));
+
+document.getElementById('button1').addEventListener('click', function () {
+  console.log(this); // <button></button>
+});
+
+document.getElementById('button1').addEventListener('click', () => {
+  console.log(this); // Window
+});
