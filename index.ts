49adc8704d0a6dcca1d8220bbeb9bc46b8d5cffb
zhouwangwang <zhouwangwang@gmail.com>
2022-06-23 15:08:39 +0900

    1 file modified

--- index.ts
+++ index.ts
@@ -1,51 +1,42 @@
-// クラス
-class Animal {
-  age: number = 10;
-  private _legs = 4;
-
-  constructor(public isCry: boolean) {
-    this.age = 2;
-  }
-
-  cry(): void {
-    if (this.isCry) {
-      alert(`age: ${this.age}`);
-    }
-  }
-
-  // アクセサメソッド(getter, setter)
-  get legs() {
-    return this._legs;
-  }
-
-  set legs(value: number) {
-    this._legs = value;
-  }
+// インターフェイス
+interface Animal {
+  name: string;
+  legs: number;
+  isCry: boolean;
+  cry(): void;
 }
 
-// let dog = new Animal(true);
-// dog.cry();
-// dog.legs = 12;
-// console.log(dog.legs);
-
-// publicは、どこからでもアクセスできる。未指定の場合はpublic
-// protectedは、自身のクラスとその派生クラスの中でアクセスできる
-// privateは、そのクラスの中でのもアクセス可能
-
-class Dog extends Animal {
-  constructor(public isCry: boolean) {
-    super(isCry);
-    this.age = 12;
-  }
-
-  cry(): void {
-    // if (this.isCry) {
-    //   alert('Bow, wow'!);
-    // }
-    console.log('dog is crying!');
-    super.cry();
-  }
+interface SuperAnimal extends Animal {
+  canRun: boolean;
 }
 
-let dog = new Dog(true);
+// class Dog implements SuperAnimal {
+//   name: string = 'Maru';
+//   legs: number = 4;
+//   isCry: boolean = true;
+//   cry(): void {
+//     if (this.isCry) {
+//       console.log('Bow, wow!');
+//     }
+//   }
+
+//   get canRun(): boolean {
+//     return false;
+//   }
+// }
+
+// // let maru = new Dog();
+// let maru: SuperAnimal = new Dog();
+// maru.canRun = true; //　効がない、Dogクラスに、canRunのsetterが定義されてないので
+// maru.cry();
+// console.log(maru.canRun);
+
+let dog: Animal = {
+  name: 'Maru',
+  legs: 4,
+  isCry: true,
+  cry: function () {
+    console.log('Bow wow!');
+  },
+};
 dog.cry();
